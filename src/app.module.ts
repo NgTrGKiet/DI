c40c5cc08708853f217b33c36b4ddd4c7675eb39
NgTrGKiet <kierngr@gmail.com>
2024-02-17 09:57:37 +0700

    base

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DiskModule } from './disk/disk.module';
+import { PowerModule } from './power/power.module';
+import { CpuModule } from './cpu/cpu.module';
+import { ComputerModule } from './computer/computer.module';
 
 @Module({
-  imports: [],
+  imports: [DiskModule, PowerModule, CpuModule, ComputerModule],
   controllers: [AppController],
   providers: [AppService],
 })
